4bd9fda82f83003bcef3dbd45a8022cd59751e00
Aaditya <aadityagupta400@gmail.com>
2023-04-13 02:20:54 +0530

    adds initial code

---
+++ index.js
@@ -0,0 +1,9 @@
+var c = 1;
+function myfun () {
+    if (c%2!=0)
+    document.getElementById("mob-links").style.display= "block"; 
+    else
+    document.getElementById("mob-links").style.display= "none";
+    c++ 
+}
+
